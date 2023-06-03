a99f56c701c290a77dc4f1cba95673e5ecdb473c
selamawit siferh <siferhselamawit@gmail.com>
2023-06-03 17:34:16 +0300

    I push phython files

---
+++ selection-sort.py
@@ -0,0 +1,13 @@
+class Solution:
+    def selectionSort(self, arr,n):
+        for i in range(0,len(arr)-1):
+            min_idx = i
+
+            for j in range(i + 1, len(arr)):
+                if arr[j] < arr[min_idx]:
+                    min_idx = j
+         
+       
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        
+        return arr
